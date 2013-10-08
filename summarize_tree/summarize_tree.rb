a8fa2aee0057b36e8067ce9f8c3d9cf91e81ea47
Gabriel Grimley <griml017@neoprene.morris.umn.edu>
2013-10-08 16:58:39 -0500

    Summarize_tree.rb completed and added.

---
+++ summarize_tree/summarize_tree.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+
+require 'find'
+
+### Create variables counting each. ###
+
+dir = ARGV[0]
+directCount = 0
+Find.find(dir) do |path|
+  if FileTest.directory?(path)
+    directCount += 1
+  end
+end
+fileCount = Dir["#{ARGV[0]}/**/*"].length
+
+
+
+puts "Processed all the files from #{dir}"
+puts "There were #{directCount} directories."
+puts "There were #{fileCount} regular files."
+
